7814ab08f025589d1179d0e5e9d682da076fa68e
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-12 17:36:06 -0700

    had a typo

--- runapp.py
+++ runapp.py
@@ -2,7 +2,7 @@ import os
 from paste.deploy import loadapp
 from waitress import serve
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
     app = loadapp('config:production.ini', relative_to='.')
 
